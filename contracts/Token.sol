c97ad2e86d16d173a6dfbb377daea9f04b3f5f70
waylen102 <139763492+waylen102@users.noreply.github.com>
2024-08-19 10:44:44 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("fff","dd"){} 
+    constructor()ERC20("fse","tew"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
